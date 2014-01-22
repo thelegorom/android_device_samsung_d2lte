2aca71d05e45442f4bbcb548805ba19c710b88be
Sean Hoyt <seanhoyt963@gmail.com>
2014-01-21 23:00:49 -0600

    Fix phone.mk

--- liquid.mk
+++ liquid.mk
@@ -4,7 +4,7 @@ $(call inherit-product, device/samsung/d2lte/full_d2lte.mk)
 $(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 
 # Inherit some common liquid stuff.
-$(call inherit-product, vendor/liquid/config/common_full_phone.mk)
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2uc \
