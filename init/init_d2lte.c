4bcff33a9c17a23c5c22ee44b4a36a68e8747e29
sspencer10 <sspencer10@gmail.com>
2014-03-18 20:15:29 -0500

    Revert "Correct ro.cdma.home.operator.numeric for VZW LTE"
    This reverts commit c0da8d06fb10265231a860b7b838daf9cb6fd0e6.

--- init/init_d2lte.c
+++ init/init_d2lte.c
@@ -146,7 +146,7 @@ void vendor_load_properties()
         property_set("ro.product.device", "d2vzw");
         property_set("ro.telephony.default_cdma_sub", "0");
         property_set("ro.cdma.home.operator.alpha", "Verizon");
-        property_set("ro.cdma.home.operator.numeric", "311480");
+        property_set("ro.cdma.home.operator.numeric", "310004");
         property_set("telephony.lteOnCdmaDevice", "1");
         property_set("ro.telephony.default_network", "10");
     } else if (strstr(bootloader, "S968C")) {
